37a8979f53d66a0c46870ce6dd8437b64dbac156
Yosep <yosep@103.234.254.186:9090>
2020-04-24 12:55:13 +0700

    commit verif controller

--- app/Http/Controllers/Pengajuan/ao/VerifController.php
+++ app/Http/Controllers/Pengajuan/ao/VerifController.php
@@ -12,11 +12,11 @@ use App\Models\AreaKantor\JPIC;
 use App\Models\AreaKantor\PIC;
 use Illuminate\Http\Request;
 use Carbon\Carbon;
-use DB;
+use Illuminate\Support\Facades\DB;
 
 class VerifController extends BaseController
 {
-    public function update($id, Request $req) 
+    public function update($id, Request $req)
     {
         $user_id  = $req->auth->user_id;
 
@@ -26,7 +26,7 @@ class VerifController extends BaseController
             return response()->json([
                 "code"    => 404,
                 "status"  => "not found",
-                "message" => "User_ID anda adalah '".$user_id."'. Namun anda belum terdaftar sebagai PIC. Harap daftarkan diri sebagai PIC pada form PIC atau hubungi bagian IT"
+                "message" => "User_ID anda adalah '" . $user_id . "'. Namun anda belum terdaftar sebagai PIC. Harap daftarkan diri sebagai PIC pada form PIC atau hubungi bagian IT"
             ], 404);
         }
 
@@ -39,11 +39,11 @@ class VerifController extends BaseController
                 "message" => "Data tidak ada"
             ], 404);
         }
-
+        //  dd($req->input('ver_ktp_debt'));
         $dataVerifikasi = array(
             'ver_ktp_debt'            => empty($req->input('ver_ktp_debt')) ? $check->ver_ktp_debt : $req->input('ver_ktp_debt'),
 
-            'ver_kk_debt'             => empty( $req->input('ver_kk_debt')) ? $check->ver_kk_debt :  $req->input('ver_kk_debt'),
+            'ver_kk_debt'             => empty($req->input('ver_kk_debt')) ? $check->ver_kk_debt :  $req->input('ver_kk_debt'),
 
             'ver_akta_cerai_debt'     => empty($req->input('ver_akta_cerai_debt')) ? $check->ver_akta_cerai_debt : $req->input('ver_akta_cerai_debt'),
 
@@ -65,13 +65,14 @@ class VerifController extends BaseController
 
             'ver_sku_debt'            => empty($req->input('ver_sku_debt')) ? $check->ver_sku_debt : $req->input('ver_sku_debt'),
 
-            'ver_pembukuan_usaha_debt'=> empty($req->input('ver_pembukuan_usaha_debt')) ? $check->ver_pembukuan_usaha_debt : $req->input('ver_pembukuan_usaha_debt'),
+            'ver_pembukuan_usaha_debt' => empty($req->input('ver_pembukuan_usaha_debt')) ? $check->ver_pembukuan_usaha_debt : $req->input('ver_pembukuan_usaha_debt'),
 
             'catatan'                 => empty($req->input('catatan')) ? $check->catatan : $req->input('catatan')
         );
 
+        //  dd($dataVerifikasi);
         DB::connection('web')->beginTransaction();
-        try{
+        try {
 
             VerifModel::where('id', $id)->update($dataVerifikasi);
 
@@ -80,7 +81,7 @@ class VerifController extends BaseController
             return response()->json([
                 'code'   => 200,
                 'status' => 'success',
-                'message'=> 'Data untuk AO berhasil dikirim',
+                'message' => 'Data untuk AO berhasil dikirim',
                 'data'   => $dataVerifikasi
                 // 'message'=> $msg
             ], 200);
